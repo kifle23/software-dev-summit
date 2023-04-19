b78941371c1f5a6f173b45a811ab9346c74194b8
kifle23 <68891198+kifle23@users.noreply.github.com>
2023-04-19 15:34:33 +0300

    milestone 5 & 6

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,13 +1,13 @@
-document.querySelector(".menu-button").addEventListener("click", () => {
-  document.querySelector(".mobile-menu").classList.toggle("show");
-  document.querySelector(".menu-button").classList.toggle("close-icon");
+document.querySelector('.menu-button').addEventListener('click', () => {
+  document.querySelector('.mobile-menu').classList.toggle('show');
+  document.querySelector('.menu-button').classList.toggle('close-icon');
 });
 
-const navItems = document.querySelectorAll(".mobile-menu li a");
+const navItems = document.querySelectorAll('.mobile-menu li a');
 
 navItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    document.querySelector(".mobile-menu").classList.toggle("show");
-    document.querySelector(".menu-button").classList.toggle("close-icon");
+  item.addEventListener('click', () => {
+    document.querySelector('.mobile-menu').classList.toggle('show');
+    document.querySelector('.menu-button').classList.toggle('close-icon');
   });
 });
